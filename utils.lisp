7165f7eef467a78773d85d299571290c24bf49e8
Pranav Ravichandran <me@onloop.net>
2013-08-02 11:05:52 +0530

    Populatory commit. Initial utils module.

---
+++ utils.lisp
@@ -0,0 +1,28 @@
+(defpackage :cl-smaw
+  (:use :cl :drakma :yason))
+
+(in-package :cl-smaw)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+(defparameter *lookup* "http://ws.spotify.com/lookup/1/.json?uri=~a")
+(defparameter *search* "http://ws.spotify.com/search/1/~a.json?q=~a"))
+
+(defun get-json-response (url)
+  (yason:parse
+   (drakma:http-request url
+			:method :get
+			:preserve-uri t
+			:want-stream t)))
+
+(defmacro construct_url (lookup-or-search params &optional (type "track"))
+  `(format nil
+	   ,(case lookup-or-search
+		  ((lookup)
+		   *lookup*
+		    (drakma:url-encode params :utf-8))
+		  ((search)
+		   *search*
+		    type
+		    (drakma:url-encode params :utf-8)))))
+
+
