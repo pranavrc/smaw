072596cead374d3ba2a60bc6bf08ab7eefa773ca
Pranav Ravichandran <me@onloop.net>
2013-08-03 12:13:45 +0530

    Function to parse json and pretty-print it.

--- utils.lisp
+++ utils.lisp
@@ -6,3 +6,17 @@
                  :method :get
                  :preserve-uri t
                  :want-stream t)))
+
+(defun extract-values (json type &optional (detailed nil))
+  (let* ((list-of-tracks (gethash type json)))
+    (if detailed
+	(mapcar #'(lambda (track artists uri) (format nil "~a, By: ~a, URI: ~a"
+						      track 
+						      (format nil "~{~a~^ and ~}" artists)
+						      uri))
+		(mapcar #'(lambda (each) (gethash "name" each)) list-of-tracks)
+		(mapcar #'(lambda (each-list) (mapcar #'(lambda (each)
+							  (gethash "name" each)) each-list))
+			(mapcar #'(lambda (each) (gethash "artists" each)) list-of-tracks))
+		(mapcar #'(lambda (each) (gethash "href" each)) list-of-tracks))
+	(mapcar #'(lambda (each) (gethash "href" each)) list-of-tracks))))
