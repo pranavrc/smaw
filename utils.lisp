2424c07c695523cc0035dd0d259baae569f759dc
Pranav Ravichandran <me@onloop.net>
2013-08-04 22:44:57 +0530

    Functions for album, track and artist parsing.

--- utils.lisp
+++ utils.lisp
@@ -9,8 +9,8 @@
                  :want-stream t)))
 
 ;; Extract Name, URI and Artist values from the JSON.
-(defun extract-values (json type &optional (pretty nil))
-  (let* ((list-of-tracks (gethash type json)))
+(defun album-and-track-parser (json albums-or-tracks &optional (pretty nil))
+  (let* ((list-of-tracks (gethash albums-or-tracks json)))
     (if pretty
 	(mapcar #'(lambda (track artists uri) (format nil "~a, By: ~a, URI: ~a"
 						      track 
@@ -27,3 +27,48 @@
 						   (gethash "name" each-list))
 					       (gethash "artists" each))
 				       :uri (gethash "href" each))) list-of-tracks))))
+
+;; Parses information from a JSON response for album queries.
+(defun album-parser (album-object)
+  (list :name (gethash "name" album-object)
+	:popularity (gethash "popularity" album-object)
+	:external-ids (mapcar #'(lambda (each) (list
+						:type (gethash "type" each)
+						:id (gethash "id" each)))
+			      (gethash "external-ids" album-object))
+	:href (gethash "href" album-object)
+	:artists (mapcar #'(lambda (each) (list
+					   :href (gethash "href" each)
+					   :name (gethash "name" each)))
+			 (gethash "artists" album-object))
+	:availability (list :territories (gethash "territories"
+						  (gethash "availability" album-object)))))
+
+;; Parses information from a JSON response for track queries.
+(defun track-parser (track-object)
+  (list :name (gethash "name" track-object)
+	:album (let* ((album-key (gethash "album" track-object)))
+		 (list :name (gethash "name" album-key)
+		       :released (gethash "released" album-key)
+		       :href (gethash "href" album-key)
+		       :availability (list :territories (gethash "territories"
+								 (gethash "availability" album-key)))))
+	:popularity (gethash "popularity" track-object)
+	:external-ids (mapcar #'(lambda (each) (list
+						:type (gethash "type" each)
+						:id (gethash "id" each)))
+			      (gethash "external-ids" track-object))
+	:length (gethash "length" track-object)
+	:href (gethash "href" track-object)
+	:artists (mapcar #'(lambda (each) (list
+					   :href (gethash "href" each)
+					   :name (gethash "name" each)))
+			 (gethash "artists" track-object))
+	:track-number (gethash "track-number" track-object)))
+
+;; Parses information from a JSON response for artist queries.
+(defun artist-parser (artist-object)
+  (list :name (gethash "name" artist-object)
+	:popularity (gethash "popularity" artist-object)
+	:href (gethash "href" artist-object)))
+	
