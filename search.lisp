e4e7e4679d7ba87b8ed2d7dd1b3199222a27b652
Pranav Ravichandran <me@onloop.net>
2013-08-05 18:59:35 +0530

    Adapting to make-plist changes.

--- search.lisp
+++ search.lisp
@@ -3,35 +3,30 @@
 ;; Parses information from a JSON response for album queries.
 (defun album-search-parser (album-search-object)
   (let* ((toplevel-keys (make-plist album-search-object
-				    :name :popularity :href)))
-    (setf (getf toplevel-keys :external-ids)
-	  (mapcar #'(lambda (each) (make-plist each :type :id))
-		  (gethash "external-ids" album-search-object))
-	  (getf toplevel-keys :artists)
-	  (mapcar #'(lambda (each) (make-plist each :href :name))
-		  (gethash "artists" album-search-object))
-	  (getf toplevel-keys :availability)
-	  (make-plist (gethash "availability" album-search-object) :territories))
+				    :name :popularity :href
+				    :external-ids :artists :availability)))
     toplevel-keys))
 
 ;; Parses information from a JSON response for track queries.
 (defun track-search-parser (track-search-object)
   (let* ((toplevel-keys (make-plist track-search-object
-				    :name :popularity :length :href :track-number)))
+				    :name :popularity :length :href :track-number
+				    :artists :external-ids)))
     (setf (getf toplevel-keys :album)
-	  (let* ((album-key (gethash "album" track-search-object)))
-	    (let* ((album-properties (make-plist album-key :name :released :href)))
-	      (setf (getf album-properties :availability)
-		    (make-plist (gethash "availability" album-key) :territories))
-	      album-properties))
-	  (getf toplevel-keys :external-ids)
-	  (mapcar #'(lambda (each) (make-plist each :type :id))
-		  (gethash "external-ids" track-search-object))
-	  (getf toplevel-keys :artists)
-	  (mapcar #'(lambda (each) (make-plist each :href :name))
-		  (gethash "artists" track-search-object)))
+	  (make-plist (gethash "album" track-search-object) :name :released
+		      :href :availability))
     toplevel-keys))
 
 ;; Parses information from a JSON response for artist queries.
 (defun artist-search-parser (artist-search-object)
   (make-plist artist-search-object :name :popularity :href))
+
+;; Parses information from a JSON response for track lookups.
+(defun track-lookup-parser (track-lookup-object)
+  (let* ((toplevel-keys (make-plist track-lookup-object :name :available 
+				    :popularity :length :href :track-number
+				    :external-ids :artists :availability)))
+    (setf (getf toplevel-keys :album)
+	  (make-plist (gethash "album" track-lookup-object)
+		      :name :released :href))
+    toplevel-keys))
