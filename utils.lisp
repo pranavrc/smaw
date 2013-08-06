2076b9cabff07bf05fd8e12bf44a5d0b7392db98
Samuel Chase <samebchase@gmail.com>
2013-08-06 23:33:45 +0530

    Untabify
    Minor cleanups.

--- utils.lisp
+++ utils.lisp
@@ -2,65 +2,66 @@
 
 (enable-interpol-syntax)
 
-;; Get JSON response from an URL.
 (defun get-json-response (url)
+  "Gets JSON response from a URL."
   (parse
    (http-request url
                  :method :get
                  :preserve-uri t
                  :want-stream t)))
 
-;; Generic function to construct a search or lookup URL.
-(defun construct-url (search-or-lookup parameters &optional (type "track"))
+
+(defun construct-url (url-type parameters &optional (type "track"))
+  "Constructs a URL."
   (let* ((encoded-parameters (url-encode parameters :utf-8))
-         (search-url #?"http://ws.spotify.com/search/1/${type}.json?q=${encoded-parameters}")
-	 (track-lookup-url #?"http://ws.spotify.com/lookup/1/.json?uri=${encoded-parameters}")
-         (album-lookup-url #?"http://ws.spotify.com/lookup/1/.json?uri=${encoded-parameters}&extras=trackdetail")
-	 (artist-lookup-url #?"http://ws.spotify.com/lookup/1/.json?uri=${encoded-parameters}&extras=albumdetail"))
-    (case search-or-lookup
+         (search-url        #?"http://ws.spotify.com/search/1/${type}.json?q=${encoded-parameters}")
+         (track-lookup-url  #?"http://ws.spotify.com/lookup/1/.json?uri=${encoded-parameters}")
+         (album-lookup-url  #?"http://ws.spotify.com/lookup/1/.json?uri=${encoded-parameters}&extras=trackdetail")
+         (artist-lookup-url #?"http://ws.spotify.com/lookup/1/.json?uri=${encoded-parameters}&extras=albumdetail"))
+    (case url-type
       (search search-url)
       (track-lookup track-lookup-url)
       (album-lookup album-lookup-url)
       (artist-lookup artist-lookup-url)
-      (otherwise (error "Specify either 'search or 'lookup symbol.")))))
+      (otherwise (error "Specify one of: '(search track-lookup album-lookup artist-lookup)")))))
 
-;; Make a property list out of a list of keys in a JSON object.
+
 (defun make-plist (json-object &rest keys)
-  (let* ((list-object ()))
+  "Makes a property list out of a list of keys in a JSON object."
+  (let ((list-object ()))
     (loop for each-key in keys do
-	 (setf (getf list-object each-key)
-	       (case each-key
-		 (:artists (mapcar #'(lambda (each) (make-plist each :href :name))
-				   (gethash "artists" json-object)))
-		 (:external-ids (mapcar #'(lambda (each) (make-plist each :type :id))
-					(gethash "external-ids" json-object)))
-		 (:availability (make-plist (gethash "availability" json-object) :territories))
-		 (otherwise (gethash (string-downcase (string each-key)) json-object)))))
+         (setf (getf list-object each-key)
+               (case each-key
+                 (:artists (mapcar #'(lambda (each) (make-plist each :href :name))
+                                   (gethash "artists" json-object)))
+                 (:external-ids (mapcar #'(lambda (each) (make-plist each :type :id))
+                                        (gethash "external-ids" json-object)))
+                 (:availability (make-plist (gethash "availability" json-object) :territories))
+                 (otherwise (gethash (string-downcase (string each-key)) json-object)))))
     list-object))
 
-;; Album search and lookup.
+
 (defun album-search (search-term)
   (mapcar #'(lambda (each) (album-search-parser each))
-	  (gethash "albums"
-		   (get-json-response (construct-url 'search search-term "album")))))
+          (gethash "albums"
+                   (get-json-response (construct-url 'search search-term "album")))))
 (defun album-lookup (lookup-term)
   (album-lookup-parser (gethash "album"
-				(get-json-response (construct-url 'album-lookup lookup-term "album")))))
+                                (get-json-response (construct-url 'album-lookup lookup-term "album")))))
 
-;; Artist search and lookup.
 (defun artist-search (search-term)
   (mapcar #'(lambda (each) (artist-search-parser each))
-	  (gethash "artists"
-		   (get-json-response (construct-url 'search search-term "artist")))))
+          (gethash "artists"
+                   (get-json-response (construct-url 'search search-term "artist")))))
 (defun artist-lookup (lookup-term)
   (artist-lookup-parser (gethash "artist"
-				 (get-json-response (construct-url 'artist-lookup lookup-term "artist")))))
+                                 (get-json-response (construct-url 'artist-lookup lookup-term "artist")))))
 
-;; Track search and lookup.
 (defun track-search (search-term)
   (mapcar #'(lambda (each) (track-search-parser each))
-	  (gethash "tracks"
-		   (get-json-response (construct-url 'search search-term)))))
+          (gethash "tracks"
+                   (get-json-response (construct-url 'search search-term)))))
+
 (defun track-lookup (lookup-term)
   (track-lookup-parser (gethash "track"
-				(get-json-response (construct-url 'track-lookup lookup-term)))))
+                                (get-json-response (construct-url 'track-lookup lookup-term)))))
