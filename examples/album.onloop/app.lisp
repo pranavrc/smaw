2be1ac0ccab788c502861f45eb42d43cf0a0e053
Pranav Ravichandran <me@onloop.net>
2013-08-09 18:03:47 +0530

    route for album lookup

--- examples/album.onloop/app.lisp
+++ examples/album.onloop/app.lisp
@@ -10,4 +10,13 @@
 (restas:define-route css ("index.css")
   (pathname "~/workbase/cl-smaw/examples/album.onloop/res/index.css"))
 
+(restas:define-route query ("album/:(query)/l")
+  (let ((params (album-onloop::string-split query #\!)))
+    (if (cl-smaw::is-uri (first params))
+	(cl-smaw::album-lookup-html (cl-smaw::album-lookup (first params)))
+	(if (second params)
+	    (cl-smaw::album-search-html (nth (- (parse-integer (second params)) 1)
+				    (cl-smaw::album-search (first params))))
+	    (cl-smaw::album-search-html (first (cl-smaw::album-search (first params))))))))
+
 (restas:start '#:restas.album-onloop :port 8080)
