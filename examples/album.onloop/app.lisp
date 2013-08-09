9ce8cb08e5252471e32669d55daf2f26d2543cd7
Pranav Ravichandran <me@onloop.net>
2013-08-09 20:15:51 +0530

    Dealing with album/artist/track urls.

--- examples/album.onloop/app.lisp
+++ examples/album.onloop/app.lisp
@@ -4,19 +4,59 @@
 
 (restas:debug-mode-on)
 
+(defmacro response-template (&body response)
+  `(who:with-html-output-to-string (*standard-output* nil :prologue t)
+     (:html
+      (:head
+       (:meta :charset "utf-8")
+       (:title "QuickHist")
+       (:link :rel "stylesheet" :href (restas:genurl 'css))
+       (:link :rel "shortcut icon" :type "image/x-icon" :href (restas:genurl 'favicon)))
+      (:body
+       (:p :id "response"
+	   ,@response)))))
+
 (restas:define-route main ("")
   (pathname "~/workbase/cl-smaw/examples/album.onloop/res/index.html"))
 
 (restas:define-route css ("index.css")
   (pathname "~/workbase/cl-smaw/examples/album.onloop/res/index.css"))
 
-(restas:define-route query ("album/:(query)/l")
-  (let ((params (album-onloop::string-split query #\!)))
-    (if (cl-smaw::is-uri (first params))
-	(cl-smaw::album-lookup-html (cl-smaw::album-lookup (first params)))
-	(if (second params)
-	    (cl-smaw::album-search-html (nth (- (parse-integer (second params)) 1)
-				    (cl-smaw::album-search (first params))))
-	    (cl-smaw::album-search-html (first (cl-smaw::album-search (first params))))))))
+(restas:define-route favicon ("favicon.ico")
+  (pathname "~/workbase/cl-smaw/examples/album.onloop/res/favicon.ico"))
+
+(restas:define-route query (":(type)/:(query)/l")
+  (response-template 
+    (:div :id "response"
+	  (who:str
+	   (handler-case
+	       (let* ((params (album-onloop::string-split query #\!)))
+		 (if (cl-smaw::is-uri (first params))
+		     (cond
+		       ((string= type "album") (cl-smaw::album-lookup-html (cl-smaw::album-lookup (first params))))
+		       ((string= type "artist") (cl-smaw::artist-lookup-html (cl-smaw::artist-lookup (first params))))
+		       ((string= type "track") (cl-smaw::track-lookup-html (cl-smaw::track-lookup (first params))))
+		       (t "Invalid URL."))
+		     (cond
+		       ((string= type "album")
+			(if (second params)
+			    (cl-smaw::album-lookup-html (cl-smaw::album-lookup (getf (nth (- (parse-integer (second params)) 1)
+											  (cl-smaw::album-search (first params))) :href)))
+			    (cl-smaw::album-lookup-html (cl-smaw::album-lookup (getf (first (cl-smaw::album-search (first params))) :href)))))
+		       ((string= type "artist")
+			(if (second params)
+			    (cl-smaw::artist-lookup-html (cl-smaw::artist-lookup (getf (nth (- (parse-integer (second params)) 1)
+											    (cl-smaw::artist-search (first params))) :href)))
+			    (cl-smaw::artist-lookup-html (cl-smaw::artist-lookup (getf (first (cl-smaw::artist-search (first params))) :href)))))
+		       ((string= type "track")
+			(if (second params)
+			    (cl-smaw::track-lookup-html (cl-smaw::track-lookup (getf (nth (- (parse-integer (second params)) 1)
+											  (cl-smaw::track-search (first params))) :href)))
+			    (cl-smaw::track-lookup-html (cl-smaw::track-lookup (getf (first (cl-smaw::track-search (first params))) :href)))))
+		       (t "Invalid URL."))))
+	     (error (e) "Oops, something went wrong."))))))
+
+(restas:define-route not-found ("*any")
+  (response-template (:div (who:str "Invalid URL."))))
 
 (restas:start '#:restas.album-onloop :port 8080)
