e82c1a8fc12d1b0b6c3d22bc4cec54e42c632bf2
Pranav Ravichandran <me@onloop.net>
2013-08-09 09:27:28 +0530

    cruft

--- utils.lisp
+++ utils.lisp
@@ -39,30 +39,6 @@
                  (otherwise (gethash (string-downcase (string key)) json-object)))))
     list-object))
 
-(defun format-plist (plist &optional (include-keys? t) &rest keys-to-include)
-  "Recursive function to pretty-print the plist returned by searches and lookups."
-  (loop for (key value) on plist by #'cddr
-     when (member key
-                  (if keys-to-include
-                      keys-to-include
-                      plist) :test #'equal)
-     collect (cond
-               ((member key '(:album :availability) :test #'equal)
-                (if include-keys?
-                    (format nil "~a:~a" (string key) (format-plist value include-keys?))
-                    (format nil "~a" (format-plist value include-keys?))))
-
-               ((member key '(:external-ids :artists :tracks :albums) :test #'equal)
-                (if include-keys?
-                    (format nil "~a:~a" (string key)
-                            (mapcar #'(lambda (each) (format-plist each include-keys?)) value))
-                    (format nil "~a" (mapcar #'(lambda (each) (format-plist each include-keys?)) value))))
-
-               (t
-                (if include-keys?
-                    (format nil "~a:~a" key value)
-                    (format nil "~a" value))))))
-
 (defun filter-results (results &rest filters)
   "Generates a list of results that match only the specified results (key-value pairs)."
   (remove nil
@@ -116,7 +92,3 @@
   (track-lookup-parser (gethash "track"
                                 (get-json-response
                                  (construct-url 'track-lookup lookup-term)))))
-
-(defun generate-embed-html (uri &key (width 300) (height 380))
-  "Generate HTML markup for embedding spotify widgets."
-  #?|<iframe src="https://embed.spotify.com/?uri=${uri}" width="${width}" height="${height}" frameborder="0" allowtransparency="true"></iframe>|)
